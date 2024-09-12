912041e2d7ca45da8a8e97ac16622bb8cde6c0e6
Val Rakitine <vrakitine@gmail.com>
2024-09-11 20:39:59 -0400

    Updated  text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,39 @@ export default function Home() {
       <Hero />
       <SectionTitle
         preTitle="Eco-programming Benefits"
-        title=" Why should you use this methodology"
+        title="We are pleased to announce the establishment of a new public movement aimed at supporting and promoting an environmentally responsible approach to programming — the Eco-Programming Alliance (EPA)."
       >
-        Eco-programming is more than just an approach to development — it`s a
-        commitment to the future. By adopting this methodology, you not only
-        create efficient, adaptable software but also minimize environmental
-        impact.
+<p>Dear Colleagues,</p>
+
+<p>
+
+Eco-programming is a <a href="https://medium.com/@vrakitine/introduction-to-eco-programming-paradigm-9f7f2c972acd">programming paradigm</a> based on the idea that any software product is ultimately useless and harmful to humanity, except for software that can be quickly and inexpensively reprogrammed (modified, updated) to work with new data in new conditions.
+
+We are proud to announce that the Eco-Programming Alliance (EPA) already includes three foundational methodologies:</p>
+<ul>
+  <li>Automata-Based Programming</li>
+  <li>V-Agent Oriented Programming (VAOP)</li>
+  <li>Model-Driven Engineering</li>
+</ul>
+
+<p>These methodologies are already operating within the new eco-programming paradigm, emphasizing sustainability and environmental responsibility in software development.</p>These methodologies are already operating within the new eco-programming paradigm, emphasizing sustainability and environmental responsibility in software development.
+
+<p>Call to Action:</p>
+
+<p>We invite all interested organizations and individuals who have developed technologies or programming methodologies that align with the principles of eco-programming to join us. Joining the Eco-Programming Alliance will provide you with a platform for knowledge exchange, collaboration, and the promotion of environmentally responsible practices in programming.</p>
+<p>Goals and Objectives of the Alliance:</p>
+
+
+<ul>
+  <li>- Raise Awareness: Spread the ideas of eco-programming among developers and companies.</li>
+  <li>- Promote Best Practices: Facilitate the adoption of methodologies and technologies that support eco-programming principles.</li>
+  <li>- Build a Community: Unite like-minded individuals for knowledge, experience, and resource sharing.</li>
+  <li>- Education and Development: Conduct seminars, courses, and training to disseminate environmentally responsible practices.</li>
+  <li>- Influence the Industry: Impact companies and developers to implement sustainable practices.</li>
+</ul>
+
+<p>Join Us!</p>
+
       </SectionTitle>
 
       <Benefits data={benefitOne} />
